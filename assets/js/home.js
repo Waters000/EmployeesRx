78e15c84f15ec21b82b450f455b5e2446269986d
Chris Waters <Floridarealty001@gmail.com>
2023-03-14 14:21:22 -0400

    firstDraft

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,17 +1,40 @@
 // Get the modal
 var modal = document.getElementById("myModal");
 
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 var prescriptionsEl = document.getElementsByClassName("prescriptions");
-
-
-
 var pricingImage = document.getElementById("pricingImage");
 var indivPricing = document.getElementById("indivPricing");
+var specialWordEl = document.getElementById("specialWord");
 
 
+
+var specialWord = ["Future ", "Start | Cancel anytime ", "Netflix ", "Best ", "Solution ", "New Way " ]
+var index = 0;
+
+function spcecialWord() {
+
+  var arrayLength = (specialWord.length - 1)
+
+  if (index < arrayLength) { 
+    index++;
+     
+  } else {
+    index = 0
+  }
+  
+  var word = specialWord[index]
+ 
+
+  specialWordEl.textContent = word
+
+
+
+
+//console.log(word)
+
+}
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
@@ -26,20 +49,26 @@ window.onclick = function(event) {
   }
 }
 
+// shows modal when starting to search
 function searchFake() {
     modal.style.display = "block";
     return
   }
 
-
-  function businessBtn() {
+// shows business pricing plan
+function businessBtn() {
     pricingImage.style.display = "block"
     indivPricing.style.display = "none"
    
     }
 
-    function individualBtn() {
+    // shows individual pricing plan
+  function individualBtn() {
         pricingImage.style.display = "none"
         indivPricing.style.display = "block"
         }
 
+
+
+      spcecialWord()
+        setInterval(spcecialWord, 5000);
